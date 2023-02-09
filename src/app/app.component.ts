60fb8023063c6af0cb34806acb50b6938b141916
mireyachaparro <mireyacn99@gmail.com>
2023-02-09 13:09:36 +0100

    Clean code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Observable, Subscribable, Subscription } from 'rxjs';
-import { Characters } from 'src/utils/interfaces';
-import { ApiService } from './services/api.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -11,23 +7,8 @@ import { ApiService } from './services/api.service';
 })
 export class AppComponent {
   title = 'R Y M';
-  // counter = 0;
-
-  // increment() {
-  //   this.counter++;
-  // }
-
-  // decrement() {
-  //   this.counter--;
-  // }
-
-  // form = new FormGroup({
-  //   name: new FormControl('', Validators.required), //esto tb se tiene que importar FormControl({}), y el validators
-  //   surname: new FormControl('', Validators.required),
-  // }); //te pide importar FormGroup({})
 
   // this.characters$ = this.service.getCharacters().subscribe((data) => {
   //   console.log(data);
   // });
 }
-// }
